a4e75aa2f115781d49449f4dd2f0b9730f5be1ff
Ahmed Aredah <aredahahmed@gmail.com>
2023-08-07 18:15:45 -0700

    new

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function that prints the program name
+ * @argc: argument counter
+ * @argv: argument array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[]) 
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: arguments counter
+ * @argv: arguments values
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, result = 0;
+	if (argc != 3)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (! isdigit(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		result += atoi(argv[i]);
+	}
+	printf("%d\n", result);
+	return (0);
+}
