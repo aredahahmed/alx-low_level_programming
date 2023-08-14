45a049a106ee710b34ddb5fa56d1239cc5d91f6c
Ahmed Aredah <aredahahmed@gmail.com>
2023-08-14 16:56:11 -0700

    fix structure

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct dog
+ * @name: pointer to char name's dog
+ * @age: float age's dog
+ * @owner: pointer to char owner's dog
+ */
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
