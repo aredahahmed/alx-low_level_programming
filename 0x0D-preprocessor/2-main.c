784491aabfa6d613c13b942bb238be2270fcd2ce
Ahmed Aredah <aredahahmed@gmail.com>
2023-08-14 16:38:01 -0700

    fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - function
- * 
- * @Return: 0
+ * main - check code
+ *
+ * Return: Always 0
  */
+
 int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
