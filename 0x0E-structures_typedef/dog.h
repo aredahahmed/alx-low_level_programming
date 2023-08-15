14e6263fca2943c1c7c2aa8211b706dd32def83c
Ahmed Aredah <aredahahmed@gmail.com>
2023-08-14 17:33:17 -0700

    fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,23 +1,30 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef FILE_DOG
+#define FILE_DOG
 
 /**
- * struct dog - structure of dog
- * @name: a pointer to char dog's name
- * @age: a float for the dog's age
- * @owner: pointer to char for the dog's owner
+ * struct dog - structure dog
+ * @name: atribute pointer to char name's dog
+ * @age: atribute float age's dog
+ * @owner: atribute pointer to char owner's dog
  */
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
 
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
 void print_dog(struct dog *d);
+
 dog_t *new_dog(char *name, float age, char *owner);
+
 void free_dog(dog_t *d);
 
 #endif
+
+
