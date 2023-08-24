7442d0c9db7ae1bc4889d8621087ca33c42bc074
Ahmed Aredah <aredahahmed@gmail.com>
2023-08-24 16:18:55 -0700

    fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,9 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - prints all elements in a list
- * @h: pointer to a list
- * return: the number of printer nodes
+ * print_list - prints all the elements of a linked list
+ * @h: pointer to the list_t list to print
+ *
+ * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
 {
@@ -13,18 +14,14 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
-		
 		h = h->next;
 		n++;
-	
 	}
 
 	return (n);
 }
+
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,23 +1,29 @@
 #ifndef LISTS_H
 #define LISTS_H
-
+#include <stdlib.h>
+#include <string.h>
 /**
- * struct list_t - singly linked list
- * @str: The string to be saved
- * @len: The length of the string
- * @next: The pointer to the next node
+ * struct list_s - singly linked list
+ * @str: The saved string
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX  project
  */
-typedef struct list_t
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list_t *next;
+	struct list_s *next;
 } list_t;
 
-
 size_t print_list(const list_t *h);
-
-
-
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 
 #endif
+
+
